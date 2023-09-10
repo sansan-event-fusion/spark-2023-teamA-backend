ba417bdb101132b3829874440f284ff5b8316cec
nagaoka1166 <ryuu11660408@gmail.com>
2023-09-10 20:00:49 +0900

    rentalhouse && mansion && RentalHousePhotoを作成するRoute作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :owner do
       resource :signup, only: :create
       resource :login, only: :create
+      namespace :mansion do
+        resource :create, only: :create
+      end
     end
   end
 end
