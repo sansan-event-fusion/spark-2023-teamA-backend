741e74e144dc64491fb937a2d8d7e81ecf1a8ad1
Shota Mizusaki <nrxg129@gmail.com>
2023-09-11 15:50:43 +0900

    mansionのモデルを修正

--- app/models/masion_room.rb
+++ app/models/masion_room.rb
@@ -5,15 +5,15 @@ class MansionRoom < ApplicationRecord
   # バリデーションの設定
   validates :name, presence: true
   validates :layout, presence: true
-  validates :thanks_money, presence: true
-  validates :security_deposit, presence: true
-  validates :floor_number, presence: true
-  validates :available_dates, presence: true
+  validates :thanksMoney, presence: true
+  validates :securityDeposit, presence: true
+  validates :floorNumber, presence: true
+  validates :availableDates, presence: true
   validates :mansion_id, presence: true
   validates :room_status_id, presence: true
-  validates :stay_fee, presence: true
+  validates :stayFee, presence: true
   validates :rent, presence: true
-  validates :maintenance_fee, presence: true
-  validates :contract_duration, presence: true
+  validates :maintenanceFee, presence: true
+  validates :contractDuration, presence: true
 
 end
