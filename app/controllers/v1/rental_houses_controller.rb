c269e0540ea3405519b329ed2edf7311f974c94c
Shota Mizusaki <nrxg129@gmail.com>
2023-09-11 14:57:48 +0900

    blankの場合を消した

--- app/controllers/v1/rental_houses_controller.rb
+++ app/controllers/v1/rental_houses_controller.rb
@@ -1,12 +1,6 @@
 class V1::RentalHousesController < ApplicationController
   def index
     @rental_houses = RentalHouse.all
-
-    if @rental_houses.blank?
-      render_error errors: @rental_houses.errors, message: "登録はありません", status: :not_found
-    else
-      render json: @rental_houses, each_serializer: RentalHouseSerializer, status: :ok
-    end
-
+    render json: @rental_houses, each_serializer: RentalHouseSerializer, status: :ok
   end
 end
