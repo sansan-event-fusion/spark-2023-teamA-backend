2f9e96302d96dad33f31e4f0e760fc440e967776
Shota Mizusaki <nrxg129@gmail.com>
2023-09-12 19:11:12 +0900

    whereから変更

--- app/controllers/v1/owner/rental_houses_controller.rb
+++ app/controllers/v1/owner/rental_houses_controller.rb
@@ -1,7 +1,7 @@
 class V1::Owner::RentalHousesController < V1::Owner::BasesController
   def index
     # オーナー個人の持っている物件一覧を取得
-    @rental_houses = RentalHouse.where(owner_id: @current_owner.id)
+    @rental_houses = @current_owner.rental_houses
     render json: @rental_houses, each_serializer: RentalHouseSerializer, status: :ok
   end
 end
