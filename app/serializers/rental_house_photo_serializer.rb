fa27535383c90042822b7e3a9e50fcd255da3c41
Shota Mizusaki <nrxg129@gmail.com>
2023-09-12 17:26:32 +0900

    バグを修正

--- app/serializers/rental_house_photo_serializer.rb
+++ app/serializers/rental_house_photo_serializer.rb
@@ -1,7 +1,7 @@
-Class RentalHousePhotoSerializer < ActiveModel::Serializer
+class RentalHousePhotoSerializer < ActiveModel::Serializer
     belongs_to :rental_house
+
     attributes :id
     attributes :image
-
     attributes :rental_house_id
 end
