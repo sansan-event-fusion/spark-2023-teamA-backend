b91076c37f805555aaddb8adcc4752c449e0660e
Shota Mizusaki <nrxg129@gmail.com>
2023-09-13 15:31:55 +0900

    レンタルハウス1件とマンションルームを全件取得する

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       resource :login, only: :create
       resources :rental_houses, only: :index
       resources :mansions, only: :create do
-        resources :mansion_rooms, only: %i[create show]
+        resources :mansion_rooms, only: %i[create index]
       end
     end
   end
