9315ee6bce0407e5da7292c6fc3934ada0d66dc9
nagaoka1166 <ryuu11660408@gmail.com>
2023-09-14 17:14:39 +0900

    認証まわりに変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-# Rails.application.config.session_store :cookie_store,
-#                                        key: '_fill_home_app_session',
-#                                        expire_after: 24.hours,
-#                                        secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store,
+                                       key: '_fill_home_app_session',
+                                       expire_after: 24.hours,
+                                       secure: true
